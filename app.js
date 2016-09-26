784d7374523ca55cdd6e15e7f29186b0f3673a3f
Loic Serafin <loicserafin@gmail.com>
2016-09-26 17:26:12 +0200

    serving static files

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.use(express.static('vendor'));
+app.use(express.static('css'));
+app.use(express.static('js'));
+app.use(express.static('img'));
+
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
