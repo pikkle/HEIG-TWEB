6d1b014019fa68369ac16f2a15c09f5ce3acc3ae
Loic Serafin <loicserafin@gmail.com>
2016-09-26 16:47:45 +0200

    changed port for heroku

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(80);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
