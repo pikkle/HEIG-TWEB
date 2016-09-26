7536c92b6d024c9e026b53c4a0fa4a0e44009ef4
Loic Serafin <loicserafin@gmail.com>
2016-09-26 17:29:12 +0200

    Moving static files

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.use(express.static('vendor'));
-app.use(express.static('css'));
-app.use(express.static('js'));
-app.use(express.static('img'));
+app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
